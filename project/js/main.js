e06c0ef95acbf132624f3575c8b4ddadeff480a5
leannejk <leannejk1@gmail.com>
2021-09-18 11:20:43 -0500

    SVG, d32

--- project/js/main.js
+++ project/js/main.js
@@ -1,30 +1,30 @@
-var sandwiches = [
-    { name: "Thesis", price: 7.95, size: "large" },
-    { name: "Dissertation", price: 8.95, size: "large" },
-    { name: "Highlander", price: 6.50, size: "small" },
-    { name: "Just Tuna", price: 6.50, size: "small" },
-    { name: "So-La", price: 7.95, size: "large" },
-    { name: "Special", price: 12.50, size: "small" }
-];
-var svg = d3.select("body").append("svg")
-    .attr("width", 500)
-    .attr("height", 100);
-
-svg.selectAll("circle").data(sandwiches).enter().append("circle")
-    .attr("cx", function(d, index) {
-        return (50 + index * 70);})
-    .attr("cy", 50)
-    .attr("r", function (d){
-        if(d.size === "large"){
-            return 30;
-        } else return 15;
-    })
-    .attr("fill", function (d){
-        if(d.price < 7.00){
-            return "blue";
-        } else return "pink";
-    })
-    .attr("stroke", "black" )
+// var sandwiches = [
+//     { name: "Thesis", price: 7.95, size: "large" },
+//     { name: "Dissertation", price: 8.95, size: "large" },
+//     { name: "Highlander", price: 6.50, size: "small" },
+//     { name: "Just Tuna", price: 6.50, size: "small" },
+//     { name: "So-La", price: 7.95, size: "large" },
+//     { name: "Special", price: 12.50, size: "small" }
+// ];
+// var svg = d3.select("body").append("svg")
+//     .attr("width", 500)
+//     .attr("height", 100);
+//
+// svg.selectAll("circle").data(sandwiches).enter().append("circle")
+//     .attr("cx", function(d, index) {
+//         return (50 + index * 70);})
+//     .attr("cy", 50)
+//     .attr("r", function (d){
+//         if(d.size === "large"){
+//             return 30;
+//         } else return 15;
+//     })
+//     .attr("fill", function (d){
+//         if(d.price < 7.00){
+//             return "blue";
+//         } else return "pink";
+//     })
+//     .attr("stroke", "black" )
 
 var euCity;
 d3.csv("data/cities.csv")
